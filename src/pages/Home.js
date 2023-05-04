7a53111fa176603229c61b9c0f5f20fe80291f24
SanathKumaraEng <dmsksanath@gmail.com>
2023-05-04 13:38:46 +0530

    pages created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
+import Link from '@material-ui/core/Link';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="about">
+        <h2> Hi, I'm Sanath Kumara</h2>
+        <div className="prompt">
+          <p>A mechanical engineer and a software developer with a passion for learning and creating.</p>
+          <LinkedInIcon onClick={() => window.location.href='https://www.google.com'} />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
+              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
+              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+
+export default Home;
